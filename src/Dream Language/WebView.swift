d0ce5e05012ef9bb6b6005addb43ceba6e7ae80a
lukaspomp-ui <lukas.pomp@gmail.com>
2026-09-13 11:48:39 +0200

    Add files via upload

--- src/Dream Language/WebView.swift
+++ src/Dream Language/WebView.swift
@@ -14,6 +14,8 @@ func createWebView(container: UIView, WKSMH: WKScriptMessageHandler, WKND: WKNav
     userContentController.add(WKSMH, name: "push-permission-request")
     userContentController.add(WKSMH, name: "push-permission-state")
     userContentController.add(WKSMH, name: "push-token")
+    userContentController.add(WKSMH, name: "iap-purchase")
+    userContentController.add(WKSMH, name: "iap-restore")
 
     config.userContentController = userContentController
 
